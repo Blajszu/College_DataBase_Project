248e8ff5158035b5927e165e22b7082c888f4da0
Blajszu <oskar.blajsz@gmail.com>
2024-12-30 16:05:27 +0100

    Update delete script

--- deleteAllScript.sql
+++ deleteAllScript.sql
@@ -1,47 +1,76 @@
 -- Usuwanie kluczy obcych
-DECLARE @sql NVARCHAR(MAX) = (
-    SELECT 
-        STRING_AGG(
-            'ALTER TABLE ' + QUOTENAME(SCHEMA_NAME(schema_id)) + '.' + 
-            QUOTENAME(OBJECT_NAME(parent_object_id)) +
-            ' DROP CONSTRAINT ' + QUOTENAME(name) + ';',
-            CHAR(10)
-        )
-    FROM sys.foreign_keys
-);
+DECLARE @sql NVARCHAR(MAX);
+DECLARE @schema NVARCHAR(128);
+DECLARE @table NVARCHAR(128);
+DECLARE @constraint NVARCHAR(128);
 
--- Wykonanie skryptu usuwającego klucze obce
-IF @sql IS NOT NULL
+DECLARE foreignKeyCursor CURSOR FOR
+SELECT 
+    SCHEMA_NAME(schema_id),
+    OBJECT_NAME(parent_object_id),
+    name
+FROM sys.foreign_keys;
+
+OPEN foreignKeyCursor;
+FETCH NEXT FROM foreignKeyCursor INTO @schema, @table, @constraint;
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    SET @sql = 'ALTER TABLE ' + QUOTENAME(@schema) + '.' + QUOTENAME(@table) + 
+               ' DROP CONSTRAINT ' + QUOTENAME(@constraint) + ';';
+    PRINT @sql;
     EXEC sp_executesql @sql;
+
+    FETCH NEXT FROM foreignKeyCursor INTO @schema, @table, @constraint;
+END;
+
+CLOSE foreignKeyCursor;
+DEALLOCATE foreignKeyCursor;
 
 -- Usuwanie kluczy głównych
-SET @sql = (
-    SELECT 
-        STRING_AGG(
-            'ALTER TABLE ' + QUOTENAME(TABLE_SCHEMA) + '.' + 
-            QUOTENAME(TABLE_NAME) +
-            ' DROP CONSTRAINT ' + QUOTENAME(CONSTRAINT_NAME) + ';',
-            CHAR(10)
-        )
-    FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
-    WHERE CONSTRAINT_TYPE = 'PRIMARY KEY'
-);
+DECLARE primaryKeyCursor CURSOR FOR
+SELECT 
+    TABLE_SCHEMA,
+    TABLE_NAME,
+    CONSTRAINT_NAME
+FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS
+WHERE CONSTRAINT_TYPE = 'PRIMARY KEY';
 
--- Wykonanie skryptu usuwającego klucze główne
-IF @sql IS NOT NULL
+OPEN primaryKeyCursor;
+FETCH NEXT FROM primaryKeyCursor INTO @schema, @table, @constraint;
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    SET @sql = 'ALTER TABLE ' + QUOTENAME(@schema) + '.' + QUOTENAME(@table) + 
+               ' DROP CONSTRAINT ' + QUOTENAME(@constraint) + ';';
+    PRINT @sql;
     EXEC sp_executesql @sql;
+
+    FETCH NEXT FROM primaryKeyCursor INTO @schema, @table, @constraint;
+END;
+
+CLOSE primaryKeyCursor;
+DEALLOCATE primaryKeyCursor;
 
 -- Usuwanie tabel
-SET @sql = (
-    SELECT 
-        STRING_AGG(
-            'DROP TABLE ' + QUOTENAME(TABLE_SCHEMA) + '.' + QUOTENAME(TABLE_NAME) + ';',
-            CHAR(10)
-        )
-    FROM INFORMATION_SCHEMA.TABLES
-    WHERE TABLE_TYPE = 'BASE TABLE'
-);
+DECLARE tableCursor CURSOR FOR
+SELECT 
+    TABLE_SCHEMA,
+    TABLE_NAME
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_TYPE = 'BASE TABLE';
 
--- Wykonanie skryptu usuwającego tabele
-IF @sql IS NOT NULL
+OPEN tableCursor;
+FETCH NEXT FROM tableCursor INTO @schema, @table;
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    SET @sql = 'DROP TABLE ' + QUOTENAME(@schema) + '.' + QUOTENAME(@table) + ';';
+    PRINT @sql;
     EXEC sp_executesql @sql;
+
+    FETCH NEXT FROM tableCursor INTO @schema, @table;
+END;
+
+CLOSE tableCursor;
+DEALLOCATE tableCursor;
